233f21540091d39b04d24cad36ea99b67a54e2f1
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2020-02-03 21:29:24 +0100

    .

--- compiler/damlc/daml-ide-core/src/Development/IDE/Core/Rules/Daml.hs
+++ compiler/damlc/daml-ide-core/src/Development/IDE/Core/Rules/Daml.hs
@@ -80,7 +80,7 @@ import Development.IDE.Core.RuleTypes.Daml
 
 import DA.Bazel.Runfiles
 import DA.Daml.DocTest
-import DA.Daml.LFConversion (convertModule, sourceLocToRange)
+import DA.Daml.LFConversion (convertModule)
 import DA.Daml.LFConversion.UtilLF
 import DA.Daml.LF.Reader (parseUnitId)
 import qualified DA.Daml.LF.Ast as LF
