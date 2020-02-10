b4327da217e334221483b25b8501f0fe17f345eb
Robert Autenrieth <robert.autenrieth@digitalasset.com>
2020-02-10 22:56:58 +0100

    Wire command deduplication to DAO

--- ledger/sandbox/src/main/scala/com/digitalasset/platform/store/ReadOnlyLedger.scala
+++ ledger/sandbox/src/main/scala/com/digitalasset/platform/store/ReadOnlyLedger.scala
@@ -3,9 +3,11 @@
 
 package com.digitalasset.platform.store
 
+import java.time.Instant
+
 import akka.NotUsed
 import akka.stream.scaladsl.Source
-import com.daml.ledger.participant.state.index.v2.PackageDetails
+import com.daml.ledger.participant.state.index.v2.{CommandSubmissionResult, PackageDetails}
 import com.daml.ledger.participant.state.v1.Configuration
 import com.digitalasset.daml.lf.data.Ref
 import com.digitalasset.daml.lf.data.Ref.{PackageId, Party}
@@ -24,6 +26,7 @@ import com.digitalasset.ledger.api.domain.{
 import com.digitalasset.ledger.api.health.ReportsHealth
 import com.digitalasset.platform.participant.util.EventFilter.TemplateAwareFilter
 import com.digitalasset.platform.store.entries.{
+  CommandDeduplicationEntry,
   ConfigurationEntry,
   LedgerEntry,
   PackageLedgerEntry,
@@ -78,4 +81,17 @@ trait ReadOnlyLedger extends ReportsHealth with AutoCloseable {
   def lookupLedgerConfiguration(): Future[Option[(Long, Configuration)]]
   def configurationEntries(
       startInclusive: Option[Long]): Source[(Long, ConfigurationEntry), NotUsed]
+
+  /** Deduplicates commands.
+    * Returns None if this is the first time the command is submitted
+    * Returns Some(entry) if the command was submitted before */
+  def deduplicateCommand(
+      deduplicationKey: String,
+      submittedAt: Instant,
+      ttl: Instant): Future[Option[CommandDeduplicationEntry]]
+
+  def updateCommandResult(
+      deduplicationKey: String,
+      submittedAt: Instant,
+      result: CommandSubmissionResult): Future[Unit]
 }
