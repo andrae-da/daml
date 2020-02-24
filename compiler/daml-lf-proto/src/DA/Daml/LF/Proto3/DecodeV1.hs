feb61106bbdaea55bcdad7f7db2a9525ea66761e
jberthold <jost.berthold@digitalasset.com>
2020-02-25 09:56:26 +1100

    DEL-7865 remove a draft error call

--- compiler/daml-lf-proto/src/DA/Daml/LF/Proto3/DecodeV1.hs
+++ compiler/daml-lf-proto/src/DA/Daml/LF/Proto3/DecodeV1.hs
@@ -701,7 +701,7 @@ decodePrim = pure . \case
   LF1.PrimTypeINT64 -> BTInt64
   LF1.PrimTypeDECIMAL -> BTDecimal
   LF1.PrimTypeNUMERIC -> BTNumeric
-  LF1.PrimTypeBIGDECIMAL -> error "BTBigDecimal"
+  LF1.PrimTypeBIGDECIMAL -> BTBigDecimal
   LF1.PrimTypeTEXT    -> BTText
   LF1.PrimTypeTIMESTAMP -> BTTimestamp
   LF1.PrimTypePARTY   -> BTParty
